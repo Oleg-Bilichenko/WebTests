d98f67e8fb48f60647a0b65b47ae1444c7630ffa
Oleg Bill <108643410+Oleg-Bilichenko@users.noreply.github.com>
2022-07-09 21:58:04 -0400

    Status print deleted

--- src/test/java/webTest.java
+++ src/test/java/webTest.java
@@ -47,8 +47,6 @@ public class webTest {
         WebElement actualResult = driver.findElement(By.xpath("//div[@id=\"main\"]/h2"));
 
         Assert.assertEquals(actualResult.getText(), expectedResult);
-
-        System.out.println("Test finished successfully");
     }
 
 }
