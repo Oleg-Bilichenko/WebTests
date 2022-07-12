80c83398593a54281cb3f2bc14c5ba5a5c40cfdf
Oleg-Bilichenko <o.gbil.ko@gmail.com>
2022-07-12 19:04:18 -0400

    Added Test FooterGuestbook

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -275,4 +275,22 @@ public class HomePageTest {
         Assert.assertEquals(actualResult, expectedResult);
     }
 
+    @Test
+    public void checkFooterGuestbook() {
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/Users/olegbill/Disk_D/Testing/QAForEveryone/QAForEveryone_03/#qa_java_beginners/MVN_Progekts_IrinaZ/WebTests/chromedriver";
+        String url = "https://www.99-bottles-of-beer.net/";
+        String expectedResult = "https://www.99-bottles-of-beer.net/guestbookv2.html";
+
+        System.setProperty(chromeDriver, driverPath);
+        driver.get(url);
+
+        WebElement link = driver.findElement(By.xpath("//div[@id=\"footer\"]//a[@href=\"/guestbookv2.html\"]"));
+        link.click();
+
+        String actualResult = driver.getCurrentUrl();
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+
 }
