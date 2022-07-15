8d15735f05934fd37ac55a29bb29ab305a30c981
Oleg-Bilichenko <o.gbil.ko@gmail.com>
2022-07-14 20:33:39 -0400

    Added Test LinkHire

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -306,7 +306,6 @@ public class HomePageTest {
         Assert.assertEquals(actualResult, expectedResult);
     }
 
-    //    //div[@id="main"]//a[@href="./info.html"]
     @Test
     public void checkLinkHistoricInformation() {
         String chromeDriver = "webdriver.chrome.driver";
@@ -318,6 +317,23 @@ public class HomePageTest {
         driver.get(url);
 
         WebElement link = driver.findElement(By.xpath("//div[@id=\"main\"]//a[@href=\"./info.html\"]"));
+        link.click();
+
+        String actualResult = driver.getCurrentUrl();
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void checkLinkHire() {
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/Users/olegbill/Disk_D/Testing/QAForEveryone/QAForEveryone_03/#qa_java_beginners/MVN_Progekts_IrinaZ/WebTests/chromedriver";
+        String url = "https://www.99-bottles-of-beer.net/";
+        String expectedResult = "https://www.99-bottles-of-beer.net/lyrics.html";
+
+        System.setProperty(chromeDriver, driverPath);
+        driver.get(url);
+
+        WebElement link = driver.findElement(By.xpath("//div[@id=\"main\"]//a[@href=\"./lyrics.html\"]"));
         link.click();
 
         String actualResult = driver.getCurrentUrl();
