e32316b81984d97fbe246ec3bde4fcce7485aef7
Oleg-Bilichenko <o.gbil.ko@gmail.com>
2022-07-17 17:44:17 -0400

    Added Test CheckLink_0_9_BySearchText

--- src/test/java/BrowseLanguagesTest.java
+++ src/test/java/BrowseLanguagesTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class BrowseLanguagesTest {
@@ -39,6 +40,42 @@ public class BrowseLanguagesTest {
         WebElement actualResult = driver.findElement(By.xpath("//div[@id=\"main\"]/h2"));
 
         Assert.assertTrue(actualResult.getText().contains("Category"));
+    }
+
+    @Test
+    public void checkLink_0_9_ByUrl() {
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/Users/olegbill/Disk_D/Testing/QAForEveryone/QAForEveryone_03/#qa_java_beginners/MVN_Progekts_IrinaZ/WebTests/chromedriver";
+        String url = "https://www.99-bottles-of-beer.net/abc.html";
+        String expectedResult = "https://www.99-bottles-of-beer.net/0.html";
+
+        System.setProperty(chromeDriver, driverPath);
+        driver.get(url);
+
+        WebElement link = driver.findElement(By.xpath("//a[@href='0.html']"));
+        link.click();
+
+        String actualResult = driver.getCurrentUrl();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void checkLink_0_9_BySearchText() {
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/Users/olegbill/Disk_D/Testing/QAForEveryone/QAForEveryone_03/#qa_java_beginners/MVN_Progekts_IrinaZ/WebTests/chromedriver";
+        String url = "https://www.99-bottles-of-beer.net/abc.html";
+
+        System.setProperty(chromeDriver, driverPath);
+        driver.get(url);
+
+        WebElement link = driver.findElement(By.xpath("//a[@href='0.html']"));
+        link.click();
+
+        List<WebElement> resultList = driver.findElements(By.xpath("//td/a"));
+        for (int i = 0; i < resultList.size(); i++) {
+            Assert.assertTrue(Character.isDigit(resultList.get(i).getText().toLowerCase().charAt(0)));
+        }
     }
 
 }
