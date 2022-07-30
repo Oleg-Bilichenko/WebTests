6ab045ce45d136affce878f57b0a2f10692c8128
Oleg-Bilichenko <o.gbil.ko@gmail.com>
2022-07-30 19:11:45 -0400

    Added test cleaned test checkNewLanguagesThisMonth

--- src/test/java/BrowseLanguagesTest.java
+++ src/test/java/BrowseLanguagesTest.java
@@ -487,6 +487,21 @@ public class BrowseLanguagesTest {
         Assert.assertEquals(actualResult, expectedResult);
     }
 
+    @Test
+    public void checkNewLanguagesThisMonth() {
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/Users/olegbill/Disk_D/Testing/QAForEveryone/QAForEveryone_03/#qa_java_beginners/MVN_Progekts_IrinaZ/WebTests/chromedriver";
+        String url = "https://www.99-bottles-of-beer.net/toplist.html";
+
+        System.setProperty(chromeDriver, driverPath);
+        driver.get(url);
+
+        driver.findElement(By.xpath("//a[@href='./toplist_newlanguagesthismonth.html']")).click();
+        String expectedResult = "New Languages this month";
+        String actualResult = driver.findElement(By.xpath("//div[@id='main']/h2")).getText();
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
 }
 
 
